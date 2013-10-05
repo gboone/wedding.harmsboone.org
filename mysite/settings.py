b0408e6a59d1c3e5a322ad4b8223a70798ddad11
Greg Boone <boone.greg@gmail.com>
2013-10-04 23:24:46 -0400

    Heroku static file switcheroo

--- mysite/settings.py
+++ mysite/settings.py
@@ -185,9 +185,9 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'collected_static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'collected_static'),
 )
