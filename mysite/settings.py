a4be90ee0a9e330a2350b2198a228ba5549280f4
Greg Boone <boone.greg@gmail.com>
2013-10-05 13:47:31 -0400

    Foreman works, Heroku...?

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,16 +16,16 @@ MANAGERS = ADMINS
 #         # The following settings are not used with sqlite3:
 #         'USER': '',
 #         'PASSWORD': '',
-#         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',                      # Set to empty string for default.
+#         'HOST': '',     # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',     # Set to empty string for default.
 #     }
 # }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "gboone",
-        "USER": "",
-        "PASSWORD": "",
+        "USER": "gboone",
+        "PASSWORD": "excella",
         "HOST": "localhost",
         "PORT": "",
     }
@@ -172,22 +172,22 @@ LOGGING = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+# # Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
