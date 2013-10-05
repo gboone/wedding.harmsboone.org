2fb2225af6fef6a8d2c093b4651e01c7c5ba9ff3
Greg Boone <boone.greg@gmail.com>
2013-10-05 14:36:13 -0400

    staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,7 +60,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/Users/gboone/Documents/django/mysite/mysite/'
+# MEDIA_ROOT = '/Users/gboone/Documents/django/mysite/mysite/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
